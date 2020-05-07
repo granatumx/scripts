a05787f8d5f2dcf69e4f70b7f7c0f2abd1a5a547
granatumx <dgarmire@umich.edu>
2020-05-07 18:26:05 -0400

    Update

--- initGXdatabase.sh
+++ initGXdatabase.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cp setup.sql /gx-tmp/.
+cp setup.sql /tmp-gx/.
 docker run --rm -it --network=host -v gx-tmp:/tmp-gx -e PGPASSWORD=12qw -e POSTGRES_PASSWORD=12qw postgres:latest psql --port=5433 --host=127.0.0.1 --username=postgres -f /tmp-gx/setup.sql
-rm -f /gx-tmp/setup.sql
+rm -f /tmp-gx/setup.sql
