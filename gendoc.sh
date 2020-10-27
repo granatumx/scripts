1bdb064d73a72c352316ab139dd6f0f312916536
Ubuntu <zjing@umail.ucsb.edu>
2020-10-27 22:52:00 +0000

    Updated...

--- gendoc.sh
+++ gendoc.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 docker run --rm -v `pwd`:/tmp/try -v /var/run/docker.sock:/var/run/docker.sock -it granatumx/doc:1.0.0 doconce format pandoc /tmp/try/README.do.txt --github_md;
+rm -rf README.dlog
